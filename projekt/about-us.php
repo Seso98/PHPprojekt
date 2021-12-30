dd2eae7b5205ccfb22293ecdd40d26a9b607c311
Seso98 <77240660+Seso98@users.noreply.github.com>
2021-12-30 19:37:39 +0100

    Add files via upload

---
+++ projekt/about-us.php
@@ -0,0 +1,19 @@
+<?php 
+	print '
+	<body>
+		<div class="about"><h1>About Nissan GT-R</h1></div>
+		<div id="container3">
+			  <section class="left section">
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/x5hlRi0XZNw?controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </section>
+				  <section class="right section">
+
+    <p>The Nissan <b>GT-R</b> is a high-performance sports car and grand tourer produced by Nissan that was unveiled in 2007.It is the successor to the <b>Skyline GT-R</b>, although no longer part of the Skyline range itself, that name now being used for Nissan\'s luxury-sport market.
+	<br><br>The <b>GT-R</b> is an entirely new model sharing little with the <b>Skyline GT-R</b> save its signature four round tail lights. Like some later generations of the <b>Skyline GT-R</b>, the <b>GT-R</b> has all-wheel drive with a twin-turbocharged 6-cylinder engine. But the four-wheel-steering HICAS system has been removed and the former straight-6 RB26DETT engine has been replaced with a new VR38DETT V6 engine. Because of the GT-R\'s heritage, the chassis code for the all-new version has been called CBA-R35, or R35 for short (where CBA stands for the emissions standard prefix), carrying on the naming trend from previous <b>Skyline GT-R</b> generations.
+	<br><br>The <b>GT-R</b> has also retained its Skyline predecessor\'s nickname, <b>Godzilla</b>,originally given to it by the Australian motoring publication Wheels in its July 1989 edition.
+	</p>
+  </section>
+			</div>
+	</body>'
+?>
---
+++ projekt/gallery.php
@@ -0,0 +1,70 @@
+<?php 
+	print '
+	<body>
+		<h1>Gallery</h1>
+		<div class="galerija">
+		<a href="gallery/1.jpg"><figure class="sole"><img alt="gtr1" src="gallery/1.jpg" style="width: 300px; height: 225px;">
+    		<figcaption>GT-R 1<br>
+      	</figcaption>
+  		</figure></a>
+			
+		<a href="gallery/2.jpg"><figure class="sole"><img alt="gtr1" src="gallery/2.jpg" style="width: 300px; height: 225px;">
+    		<figcaption>GT-R 2<br>
+      	</figcaption>
+  		</figure></a>
+			
+		<a href="gallery/3.jpg"><figure class="sole"><img alt="gtr1" src="gallery/3.jpg" style="width: 300px; height: 225px;">
+    		<figcaption>GT-R 3<br>
+      	</figcaption>
+  		</figure></a>
+		
+			<a href="gallery/4.jpg"><figure class="sole"><img alt="gtr1" src="gallery/4.jpg" style="width: 300px; height: 225px;">
+    		<figcaption>GT-R 4<br>
+      	</figcaption>
+  		</figure></a>
+			
+			<a href="gallery/5.png"><figure class="sole"><img alt="gtr1" src="gallery/5.png" style="width: 300px; height: 225px;">
+    		<figcaption>GT-R 5<br>
+      	</figcaption>
+  		</figure></a>
+			
+		<a href="gallery/6.jpg"><figure class="sole"><img alt="gtr1" src="gallery/6.jpg" style="width: 300px; height: 225px;">
+    		<figcaption>GT-R 6<br>
+      	</figcaption>
+  		</figure></a>
+			
+		<a href="gallery/7.jpg"><figure class="sole"><img alt="gtr1" src="gallery/7.jpg" style="width: 300px; height: 225px;">
+    		<figcaption>GT-R 7<br>
+      	</figcaption>
+  		</figure></a>
+			
+			<a href="gallery/8.jpg"><figure class="sole"><img alt="gtr1" src="gallery/8.jpg" style="width: 300px; height: 225px;">
+    		<figcaption>GT-R 8<br>
+      	</figcaption>
+  		</figure></a>
+			
+			<a href="gallery/9.jpg"><figure class="sole"><img alt="gtr1" src="gallery/9.jpg" style="width: 300px; height: 225px;">
+    		<figcaption>GT-R 9<br>
+      	</figcaption>
+  		</figure></a>
+			
+		<a href="gallery/10.jpg"><figure class="sole"><img alt="gtr1" src="gallery/10.jpg" style="width: 300px; height: 225px;">
+    		<figcaption>GT-R 10<br>
+      	</figcaption>
+  		</figure></a>
+			
+		<a href="gallery/11.jpg"><figure class="sole"><img alt="gtr1" src="gallery/11.jpg" style="width: 300px; height: 225px;">
+    		<figcaption>GT-R 11<br>
+      	</figcaption>
+  		</figure></a>
+			
+		<a href="gallery/12.jpg"><figure class="sole"><img alt="gtr1" src="gallery/12.jpg" style="width: 300px; height: 225px;">
+    		<figcaption>GT-R 12<br>
+      	</figcaption>
+  		</figure></a>
+		
+		
+		
+		</div>
+	</body>'
+?>
